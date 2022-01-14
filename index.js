3f48b4c138af7f8ec502a3c76dd9f64d822ec5d0
bmarieay <marieantonettebacay@gmail.com>
2022-01-14 13:02:57 -0800

    added the GET /tweets route and an array of tweet to mimic a database

--- index.js
+++ index.js
@@ -1,16 +1,56 @@
 const express = require("express")
 const app = express();
 const path = require("path")
+const { v4: uuid } = require('uuid');
+uuid();
 const port = 3000;
 
+
+//configure ejs
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'))
 
+//array of tweets to mimic a database
+const tweetsData = [
+    {
+        id: uuid(),
+        username: 'IamMarie',
+        tweet:  'just woke up!'
+    },
+    {
+        id: uuid(),
+        username: 'bob',
+        tweet:  'Cant wait!'
+    },
+    {
+        id: uuid(),
+        username: 'Dan123',
+        tweet:  'I passed!'
+    },
+    {
+        id: uuid(),
+        username: 'mynameis',
+        tweet:  'It is so cold'
+    }
+]
 
-app.get('/', (req, res) => {
+
+
+//routes
+app.get('/', (req, res) => { //homepage
     res.render('tweets/home')
 })
 
+app.get('/tweets', (req, res) => {
+    //pass the array of objects
+    res.render('tweets/index', {tweets : tweetsData})
+
+})
+
+
+
+
+//=====
 app.listen(3000, (req, res) => {
     console.log(`Listening to port ${port}`)
 })

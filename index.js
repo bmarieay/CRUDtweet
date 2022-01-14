74bf884e71b54dcd9022bf6548e081aad811bf08
bmarieay <marieantonettebacay@gmail.com>
2022-01-14 12:47:05 -0800

    added views directory for the templates and a starting index.js file for the routing

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require("express")
+const app = express();
+const path = require("path")
+const port = 3000;
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'))
+
+
+app.get('/', (req, res) => {
+    res.render('tweets/home')
+})
+
+app.listen(3000, (req, res) => {
+    console.log(`Listening to port ${port}`)
+})
